0558d5b370858fcb77a9432ee8ff215dbf628cec
Tim Kopplin <timkopplin@hotmail.com>
2022-11-01 00:14:01 +0000

    adding urls_new

--- express_server.js
+++ express_server.js
@@ -3,14 +3,24 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+generateRandomString = () => {  
+  const randomString = (Math.random() + 1).toString(36).substring(7);
+  return randomString;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
@@ -21,6 +31,16 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+
+app.post("urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("urls/:id", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
