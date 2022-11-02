809a7ba5749ec4a09d22268d675d103d4376c7de
Tim Kopplin <timkopplin@hotmail.com>
2022-11-02 04:29:19 +0000

    adding user login and logout

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieparser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,14 +15,14 @@ const urlDatabase = {
 generateRandomString = () => {  
   const randomString = (Math.random() + 1).toString(36).substring(7);
   return randomString;
-}
+};
 
 app.get("/", (req, res) => {
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -29,7 +31,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -39,11 +41,11 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const longURL = req.body.longURL;
   urlDatabase[id] = longURL;
   res.redirect("/urls")
-})
+});
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id
@@ -62,8 +64,19 @@ app.post("/urls/new", (req, res) => {
   res.redirect(`/urls`);
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
