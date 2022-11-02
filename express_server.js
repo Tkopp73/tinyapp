afb5b297bcf844a2056a5e0b1b1366222a5cef29
Tim Kopplin <timkopplin@hotmail.com>
2022-11-02 21:34:01 +0000

    adding a register page

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cookieparser = require("cookie-parser");
 const app = express();
+const morgan = require('morgan');
 const PORT = 8080;
 
+app.use(morgan('dev'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
@@ -30,6 +32,10 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/urls/register", (req, res) => {
+  res.render("urls_register");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
@@ -38,6 +44,19 @@ app.get("/urls/:id", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
+});
+
+app.post("/urls/new", (req, res) => {
+  const shortURL = generateRandomString();
+  console.log(shortURL);
+  const longURL = req.body.longURL;
+  console.log(longURL);
+  if(longURL.includes('http://') || longURL.includes('https://')) {
+    urlDatabase[shortURL] = req.body.longURL;
+  } else {
+    urlDatabase[shortURL] = `http://${req.body.longURL}`;
+  }
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -51,17 +70,6 @@ app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id
   delete urlDatabase[id];
   res.redirect("/urls");
-});
-
-app.post("/urls/new", (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  if(longURL.includes('http://') || longURL.includes('https://')) {
-    urlDatabase[shortURL] = req.body.longURL;
-  } else {
-    urlDatabase[shortURL] = `http://${req.body.longURL}`;
-  }
-  res.redirect(`/urls`);
 });
 
 app.post("/login", (req, res) => {
