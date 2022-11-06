2167aa8f0ff8cf7f8900238c246bf10ee465bea2
Tim Kopplin <timkopplin@hotmail.com>
2022-11-06 23:16:40 +0000

    fixing and cleaning up my code to function properly and read better

--- express_server.js
+++ express_server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
-const getUserByEmail = require('./views/helper')
+const {getUserByEmail} = require('./views/helper')
+const {urlsForUser} = require('./views/helper');
+const {generateRandomString} = require('./views/helper');
 const PORT = 8080;
 
 
@@ -30,21 +32,6 @@ const users = {
   }
 };
 
-const urlsForUser = (id) => {
-  let userURLS = {};
-  for (let url in urlDatabase) {
-    if (urlDatabase[url]["userID"] === id) {
-      userURLS[url] = urlDatabase[url];
-    }
-  }
-  return userURLS;
-};
-
-const generateRandomString = () => {  
-  const randomString = (Math.random() + 1).toString(36).substring(7);
-  return randomString;
-};
-
 app.get("/", (req, res) => {
   res.redirect('/urls');
 });
@@ -53,9 +40,9 @@ app.get("/urls", (req, res) => {
   const user_id = req.session.userID;
   const templateVars = { urls: undefined, userID: undefined};
   if (!user_id) {
-    return res.redirect("/urls/login");
+    return res.redirect("/login");
   } else {
-    templateVars.urls = urlsForUser(user_id.id);
+    templateVars.urls = urlsForUser(user_id.id, urlDatabase);
     templateVars["userID"] = user_id
     res.render("urls_index", templateVars);
   }
@@ -64,42 +51,42 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const user_id = req.session.userID;
   if (!user_id) {
-    return res.redirect("/urls/login");
+    return res.redirect("/login");
   } else {
   const templateVars = { userID: req.session.userID };
   res.render("urls_new", templateVars);
   }
 });
 
-app.get("/urls/register", (req, res) => {
+app.get("/register", (req, res) => {
   const user_id = req.session.userID;
-  const templateVars = { userID: req.session.userID, user: users };
+  const templateVars = { userID: user_id, user: users };
   return res.render("urls_register", templateVars);
 });
 
-app.get("/urls/login", (req, res) => {
+app.get("/login", (req, res) => {
   const user_id = req.session.userID;
   if (user_id) {
     return res.redirect("/urls");
   } else {
-    const templateVars = { userID: req.session.userID, user: users };
+    const templateVars = { userID: user_id, user: users };
     return res.render("urls_loginpage", templateVars);
   }
 });
 
 app.get("/urls/:id", (req, res) => {
   const user_id = req.session.userID;
+  let urlid = req.params.id;
   if (!user_id) {
     res.send("ERROR: Not logged in!");
   } else {
-    console.log(urlDatabase[req.params.id].longURL);
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, userID: user_id};
+    const templateVars = { id: req.params.id, longURL: urlDatabase[urlid].longURL, userID: user_id};
     res.render("urls_show", templateVars);
   }
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
 });
 
@@ -119,7 +106,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    if (req.body.email === "") {
+    if (!req.body.email) {
       res.send("Status code 400");
     } else if (getUserByEmail(req.body.email, users)) {
       res.send("Email already in use! Status code 400");
@@ -134,13 +121,13 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/urls/new", (req, res) => {
-  const user_id = req.session.userID;
+  const user_id = req.session.userID.id;
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
   if(longURL.includes('http://') || longURL.includes('https://')) {
-    urlDatabase[shortURL] = {"longURL": longURL, userID: user_id.id};
+    urlDatabase[shortURL] = {longURL: longURL, userID: user_id};
   } else {
-    urlDatabase[shortURL] = {"longURL": `http://${longURL}`, userID: user_id.id};
+    urlDatabase[shortURL] = {longURL: `http://${longURL}`, userID: user_id};
   }
   return res.redirect("/urls");
 });
@@ -160,7 +147,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/logout", (req, res) => {
   req.session.userID = null;
-  res.redirect("/urls/login");
+  res.redirect("/login");
 });
 
 app.listen(PORT, () => {
